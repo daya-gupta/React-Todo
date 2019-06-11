d5d2e736899804c7d2a623604010e48ec2c5025c
daya-gupta <dayanandorgupta@gmail.com>
2019-06-11 18:01:50 +0530

    removing redux stuff

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import TodoEdit from './TodoEdit'
 import TodoList from './TodoList'
 import './todo.css';
@@ -16,19 +15,11 @@ class Todo extends React.Component {
         const list = [...this.state.list];
         list.push({ todo, completed: false });
         this.setState({ list });
-
-        // const list = this.props.list;
-        // list.push({ todo, completed: false });
-        // this.props.updateList(list);
     }
     toggleStatus = (index) => {
         const list = [...this.state.list];
         list[index].completed = !list[index].completed;
         this.setState({ list });
-
-        // const list = this.props.list;
-        // list[index].completed = !list[index].completed;
-        // this.props.updateList(list)
     }
     render() {
         return (
@@ -36,22 +27,9 @@ class Todo extends React.Component {
                 <h1>{this.state.salutation} {this.props.user || 'user'}, this is your Todo list!!</h1>
                 <TodoEdit addToList={this.addToList} />
                 <TodoList list={this.state.list} toggleStatus={this.toggleStatus} />
-                {/* <TodoList list={this.props.list} toggleStatus={this.toggleStatus} /> */}
             </div>
         );
     }
 }
 
 export default Todo;
-
-// const mapStateToProps = (state) => {
-//     return {
-//         list: state.list
-//     }
-// }
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         updateList: (list) => { dispatch({ type: 'UPDATE_LIST', data: list }) }
-//     }
-// }
-// export default connect(mapStateToProps, mapDispatchToProps)(Todo);
